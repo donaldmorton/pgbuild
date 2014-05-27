c7e022f88bd9c27bdbe8139cd76227020c931ef6
donald.mg92@gmail.com <donald.mg92@gmail.com>
2014-05-26 19:31:43 -0500

    init

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ C:\P001\ionict2\platforms\android\res\drawable-mdpi\icon.png \
 C:\P001\ionict2\platforms\android\res\drawable-xhdpi\icon.png \
 C:\P001\ionict2\platforms\android\res\values\strings.xml \
 C:\P001\ionict2\platforms\android\res\xml\config.xml \
+C:\P001\ionict2\platforms\android\ant-build\res\drawable\icon.png \
+C:\P001\ionict2\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+C:\P001\ionict2\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+C:\P001\ionict2\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+C:\P001\ionict2\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\P001\ionict2\platforms\android\ant-build\AndroidManifest.xml \
